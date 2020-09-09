eb7ddf93afbaeb0db9957c54144106bfac12de9b
BiagioliCassandra <biagioli.cassandra@gmail.com>
2020-09-09 10:53:38 +0200

    first commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int wayPointIndex = 0;
+
+    private float marginError = 0.03f;
+
+    private void Start()
+    {
+        target = WayPoints.points[0];
+    }
+
+    private void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if(Vector3.Distance(transform.position, target.position) <= marginError)
+        {
+            GetNextWayPoint();
+        }
+    }
+
+    private void GetNextWayPoint()
+    {
+        if(wayPointIndex >= WayPoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        wayPointIndex++;
+        target = WayPoints.points[wayPointIndex];
+    }
+}
