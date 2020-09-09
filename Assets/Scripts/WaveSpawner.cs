41b40b8f5927f214b617759a4e0ae4d2adede09a
BiagioliCassandra <biagioli.cassandra@gmail.com>
2020-09-09 15:00:19 +0200

    add wave enemies and add turrets

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private Transform enemyPrefab;
+    [SerializeField]
+    private Transform spawnPoint;
+
+    [SerializeField]
+    private float timeBetweenWaves = 5.5f;
+    private float countdown = 2.0f;
+    private float timeBetweenSpawnEnemyInWave = 0.5f;
+
+    private int waveIndex = 0;
+
+    [SerializeField]
+    private Text waveCountdownTimer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+        countdown -= Time.deltaTime;
+        waveCountdownTimer.text = Mathf.Round(countdown).ToString();
+    }
+
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(timeBetweenSpawnEnemyInWave);
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
